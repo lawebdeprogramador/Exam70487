b179b872e46c070f5bb901c6ead4eefcd83567b9
thiago-alvesp@live.com <thiago-alvesp@live.com>
2016-06-27 16:54:33 -0300

    commit de teste

---
+++ Exam70487/1.4 - Implement data storage in Windows Azure/StorageWindowsAzure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam70487._1._4___Implement_data_storage_in_Windows_Azure
+{
+    public class StorageWindowsAzure
+    {
+    }
+}
